3fe6afb5e298c257a758cae4c649edb531f14d8c
yandi <prozhw@qq.com>
2022-10-07 16:55:19 +0800

    模拟心跳服务与断线重连

--- netty-javafile/src/main/java/org/study/netty/server/MyChannelInitializer.java
+++ netty-javafile/src/main/java/org/study/netty/server/MyChannelInitializer.java
@@ -2,6 +2,7 @@ package org.study.netty.server;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
 import org.study.netty.codec.ObjDecoder;
 import org.study.netty.codec.ObjEncoder;
 import org.study.netty.domain.FileTransferProtocol;
@@ -9,6 +10,14 @@ import org.study.netty.domain.FileTransferProtocol;
 public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
+        /**
+         * 心跳监测
+         * 1、readerIdleTimeSeconds 读超时时间
+         * 2、writerIdleTimeSeconds 写超时时间
+         * 3、allIdleTimeSeconds    读写超时时间
+         * 4、TimeUnit.SECONDS 秒[默认为秒，可以指定]
+         */
+        socketChannel.pipeline().addLast(new IdleStateHandler(2, 2, 2));
         socketChannel.pipeline().addLast(new ObjDecoder(FileTransferProtocol.class));
         socketChannel.pipeline().addLast(new ObjEncoder(FileTransferProtocol.class));
         socketChannel.pipeline().addLast(new MyServerHandler());
--- netty-javafile/src/test/java/org/study/netty/NettyClientTest.java
+++ netty-javafile/src/test/java/org/study/netty/NettyClientTest.java
@@ -10,8 +10,9 @@ import java.io.File;
 public class NettyClientTest {
     public static void main(String[] args) {
         ChannelFuture channelFuture = new NettyClient().connect("127.0.0.1", 7397);
-        File file = new File("E:\\image\\zu.png");
+        //模拟心跳服务与断线重连
+        /*File file = new File("E:\\image\\zu.png");
         FileTransferProtocol fileTransferProtocol = MsgUtil.buildRequestTransferProtocol(file.getAbsolutePath(), file.getName(), file.length());
-        channelFuture.channel().writeAndFlush(fileTransferProtocol);
+        channelFuture.channel().writeAndFlush(fileTransferProtocol);*/
     }
 }
